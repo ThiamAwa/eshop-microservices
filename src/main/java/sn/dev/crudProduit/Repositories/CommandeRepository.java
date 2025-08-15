4022821bd136a309797e291a00072178cd0edd43
ThiamAwa <awa96362@gmail.com>
2025-08-15 13:24:14 +0000

    add img swagger ui

--- src/main/java/sn/dev/crudProduit/Repositories/CommandeRepository.java
+++ src/main/java/sn/dev/crudProduit/Repositories/CommandeRepository.java
@@ -1,4 +1,4 @@
-package sn.dev.crudProduit.doa;
+package sn.dev.crudProduit.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
